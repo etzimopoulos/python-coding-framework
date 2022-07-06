d9e8f3c031a6cf813c241d170042965f433e098d
Evangelos Tzimopoulos <e.tzimopoulos@gmail.com>
2022-07-06 06:50:55 +0100

    Adding ingest and persist files with their own functions. Also adding a class in main. Class initialised, files imported and functions within the class were called

--- FileProcessor/driver.py
+++ FileProcessor/driver.py
@@ -1,9 +1,26 @@
-print("This is my driver program")
+import ingest
+import persist
+
+print("This is my driver program\n")
+
+class DriverProgram:
+    def my_function(self):
+        print("Inside my function")
 
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
-    print_hi('This is the main method')
-    print_hi('PyCharm')
+    print_hi('This is the main method\n')
+    print_hi('PyCharm\n')
+    driver = DriverProgram()
+    driver.my_function()
+
+    # Create Class instances
+    reader = ingest.FileReader()
+    writer = persist.PersistData()
+
+    # Call instances' functions
+    reader.read_file()
+    writer.store_data()
