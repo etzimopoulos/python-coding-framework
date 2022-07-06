4c8346bf95a842124b690a3248b4a77f6a248484
Evangelos Tzimopoulos <e.tzimopoulos@gmail.com>
2022-07-06 06:35:02 +0100

    Day 4: Finished section 2, started Section 3 and created FileProcessor project

---
+++ FileProcessor/driver.py
@@ -0,0 +1,9 @@
+print("This is my driver program")
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('This is the main method')
+    print_hi('PyCharm')
